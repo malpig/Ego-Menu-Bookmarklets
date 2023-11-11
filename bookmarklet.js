32189f03d9974d2c2ffb00320760970e2bad2740
malpig <115734177+malpig@users.noreply.github.com>
2023-11-11 10:26:59 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,6 @@
 javascript:(function() {
   var scriptElement = document.createElement('script');
-  var sourceUrl = 'https://raw.githubusercontent.com/yeahbread/Ego-Menu-Bookmarklets/main/Menu.js';
+  var sourceUrl = 'https://raw.githubusercontent.com/malpig/Ego-Menu-Bookmarklets/main/Menu.js';
   fetch(sourceUrl)
     .then(response => response.text())
     .then(sourceCode => {
